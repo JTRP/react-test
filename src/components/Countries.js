944324d1f902677fd782dbb4ec253ed210a3765d
JTRP <jurek.trepy@gmail.com>
2022-06-28 12:23:33 +0200

    ajout de axios + petit peu de css

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 const Countries = () => {
+
+    const [data, setData] = useState([]);
+    const [hello, setHello] = useState("Hello");
+
+    // Le useEffect se joue lorsque le composant est monté
+    useEffect(() => {
+
+        axios.get('https://restcountries.com/v3.1/all').then((res) => {
+            setData(res.data);
+        });
+
+    }, [])
+
     return (
         <div className="countries">
 
             <h1>Countries</h1>
+            <h2>{ hello }</h2>
+
+            <ul>
+
+                {
+                    data.map((country) => <li>{ country.name.common }</li>)
+                }
+            </ul>
 
         </div>
     );
